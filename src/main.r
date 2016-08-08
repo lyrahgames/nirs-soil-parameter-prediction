6353165925d4236eb0e18823fafdcd9395f0f81e
lyrahgames <markuspawellek@gmail.com>
2016-08-08 12:59:31 +0200

    added model selection algorithms to utils.r forward selection simulated annealing

--- src/main.r
+++ src/main.r
@@ -16,7 +16,7 @@ obs_count <- 3
 # construct all wavelengths (only hard coded; future: has to be read from data)
 wl <- seq(1400, 2672, by = 4)
 # get matrix of reflectance values
-refl_mat <- as.matrix(soil_spec_data[,(obs_count+1):wl_count])
+refl_mat <- as.matrix(soil_spec_data[,(obs_count+1):dim(soil_spec_data)[2]])
 # get matrix of observables
 obs_mat <- as.matrix(soil_spec_data[,1:obs_count])
 # set vector of observables (easy to read and write)
@@ -25,8 +25,40 @@ n_vec <- obs_mat[,2]
 ph_vec <- obs_mat[,3]
 
 
-mlr.var(n_vec, cbind(1,refl_mat))
-mlr.var(n_vec, cbind(1,log(refl_mat)))
-mlr.var(ph_vec, cbind(1,refl_mat))
-mlr.var(ph_vec, refl_mat)
-mlr.var(ph_vec, cbind(1,log(refl_mat)))
+
+
+
+# calculate mlr variance and inverse values
+mlr_var = mlr.var(soc_vec, refl_mat)
+inv_mlr_var = 1.0 / mlr_var
+
+
+
+# mlr.var(n_vec, cbind(1,refl_mat))
+# mlr.var(n_vec, cbind(1,log(refl_mat)))
+# mlr.var(ph_vec, cbind(1,refl_mat))
+# mlr.var(ph_vec, refl_mat)
+# mlr.var(ph_vec, cbind(1,log(refl_mat)))
+
+mallows.cp(soc_vec, refl_mat, 1:dim(refl_mat)[2], inv_mlr_var)
+# fwd_sel_idx = ms.fwd.sel(soc_vec, refl_mat, inv_mlr_var)
+# fwd_sel_idx
+# mallows.cp(soc_vec, refl_mat, fwd_sel_idx, inv_mlr_var)
+# sa_idx = ms.sa(soc_vec, refl_mat, inv_mlr_var)
+# sort(sa_idx)
+# mallows.cp(soc_vec, refl_mat, sa_idx, inv_mlr_var)
+
+test_idx_vec = match(c(1416, 1420, 1436, 1468, 1480, 1492, 1516, 1536, 1540,
+	1544, 1548, 1588, 1628, 1632, 1800, 1808, 1832, 1856, 1912, 1948, 1980,
+	1984, 1988, 2012, 2052, 2064, 2072, 2088, 2104, 2132, 2144, 2148, 2168,
+	2172, 2176, 2188, 2204, 2216, 2224, 2232, 2276, 2280, 2284, 2304, 2332,
+	2340, 2348, 2360, 2416, 2420, 2428, 2436, 2472, 2492, 2496, 2508, 2512,
+	2516, 2520, 2540, 2548, 2552, 2580, 2584, 2588, 2600, 2612, 2620, 2628,
+	2632, 2648, 2672), wl)
+
+sa_idx = ms.sa(soc_vec, refl_mat, inv_mlr_var, idx_vec = test_idx_vec)
+sort(sa_idx)
+mallows.cp(soc_vec, refl_mat, sa_idx, inv_mlr_var)
+
+test_idx_vec
+mallows.cp(soc_vec, refl_mat, test_idx_vec, inv_mlr_var)
--- src/utils.r
+++ src/utils.r
@@ -7,14 +7,128 @@ hat.mat = function(design_mat){
 	design_mat %*% solve(transp_design_mat %*% design_mat) %*% transp_design_mat
 }
 
-
-# multiple linear regression variance estimator
-mlr.var = function(obs, design_mat){
+# multiple linear regression residual sum of squares (rss)
+mlr.rss = function(obs, design_mat){
 	hat_mat <- hat.mat(design_mat)
 	res <- obs - (hat_mat %*% obs)
 
 	# return
-	(t(res) %*% res) / (length(obs) - dim(design_mat)[2])
+	t(res) %*% res
 }
 
-# mallows cp for 
+# multiple linear regression variance estimator
+mlr.var = function(obs, design_mat){
+	# return
+	(mlr.rss(obs, design_mat)) / (length(obs) - dim(design_mat)[2])
+}
+
+# mallows cp
+# idx_vec describes given model
+# inv_mlr_var is the inverse variance of the given full model
+mallows.cp = function(obs, design_mat, idx_vec, inv_mlr_var){
+	# construct design matrix of given model
+	design_mat_mod <- design_mat[,idx_vec]
+
+	# return (will be returned as matrix; do not know why)
+	(mlr.rss(obs, design_mat_mod) * inv_mlr_var) + (2*length(idx_vec)) - length(obs)
+}
+
+# model selection: forward selection method
+ms.fwd.sel = function(obs, design_mat, inv_mlr_var){
+	full_idx_vec <- seq(1, dim(design_mat)[2])
+	# first column will be used every time
+	idx_vec <- 1
+	cp <- mallows.cp(obs, design_mat, idx_vec, inv_mlr_var)
+
+	repeat{
+		# vector of selection
+		sel_vec = setdiff(full_idx_vec, idx_vec)
+
+		if (length(sel_vec) == 0){
+			break
+		}
+
+		tmp_idx_vec_1 <- c(idx_vec, sel_vec[1])
+		tmp_cp_1 <- mallows.cp(obs, design_mat, tmp_idx_vec_1, inv_mlr_var)
+
+		for (i in 2:length(sel_vec)){
+			tmp_idx_vec_2 <- c(idx_vec, sel_vec[i])
+			tmp_cp_2 <- mallows.cp(obs, design_mat, tmp_idx_vec_2, inv_mlr_var)
+
+			if (tmp_cp_2 <= tmp_cp_1){
+				tmp_cp_1 <- tmp_cp_2
+				tmp_idx_vec_1 <- tmp_idx_vec_2
+			}
+		}
+
+		if (cp >= tmp_cp_1){
+			cp <- tmp_cp_1
+			idx_vec <- tmp_idx_vec_1
+		}else{
+			break
+		}
+
+		# debug information
+		print(idx_vec)
+		print(cp)
+	}
+
+	# return
+	idx_vec
+}
+
+# model selection: simulated annealing: neighbour function
+ms.sa.nbr = function(idx_vec, max_idx){
+	# get random index (1 is not used)
+	rand_idx <- sample(2:max_idx, size = 1)
+
+	if (rand_idx %in% idx_vec){
+		# delete rand_idx in idx_vec
+		nbr_idx_vec <- idx_vec[idx_vec != rand_idx]
+	}else{
+		# add rand_idx to idx_vec
+		nbr_idx_vec <- c(idx_vec, rand_idx)
+	}
+
+	# return
+	nbr_idx_vec
+}
+
+# model selection: simulated annealing: probability function
+# costs will be minimized 
+ms.sa.prob = function(old_cost, new_cost, temp){
+	# return
+	exp( (old_cost - new_cost) / temp )
+}
+
+# model selection: simulated annealing
+ms.sa = function(obs, design_mat, inv_mlr_var, idx_vec = c(1), temp = 100, alpha = 0.99, it_max = 10000, it_exit = 1200){
+	max_idx <- dim(design_mat)[2]
+	old_cost <- mallows.cp(obs, design_mat, idx_vec, inv_mlr_var);
+	it_same <- 0
+
+	for (i in 1:it_max){
+		nbr_idx_vec <- ms.sa.nbr(idx_vec, max_idx);
+		new_cost <- mallows.cp(obs, design_mat, nbr_idx_vec, inv_mlr_var)
+
+		if ( ms.sa.prob(old_cost, new_cost, temp) >= runif(1) ){
+			idx_vec <- nbr_idx_vec
+			old_cost <- new_cost
+			it_same <- 0
+		}else{
+			it_same <- it_same + 1
+			if (it_same >= it_exit){
+				break
+			}
+		}
+
+		temp <- alpha * temp
+
+		# debug information
+		print(idx_vec)
+		print(old_cost)
+	}
+
+	# return
+	idx_vec
+}
