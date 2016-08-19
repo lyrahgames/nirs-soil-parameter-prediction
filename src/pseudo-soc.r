456d5da12a56285402c24dde49e13a35706cf9f8
lyrahgames <markuspawellek@gmail.com>
2016-08-19 12:42:28 +0200

    generated pseudo observations

---
+++ src/pseudo-soc.r
@@ -0,0 +1,25 @@
+# load source files
+source("utils.r")
+source("init.r")
+
+
+init.data(soc_vec, soc_design_mat)
+mlr.init()
+
+idx_vec <- as.vector(read.csv("../pro-files/data/gen/ms-sa-soc-idx-vec.csv", header=F)[,1])
+
+idx_vec
+length(idx_vec)
+
+ms.init.dist(idx_vec)
+
+pseudo_obs_mat <- rnorm(gv_sample_size, gv_expect_vec, gv_sd)
+
+for (i in 2:1000){
+	pseudo_obs_mat <- cbind(pseudo_obs_mat, rnorm(gv_sample_size, gv_expect_vec, gv_sd))
+	print(dim(pseudo_obs_mat))
+}
+
+write.table(pseudo_obs_mat, "../pro-files/data/gen/pseudo-obs.csv", sep="\t", col.names=F, row.names=F)
+
+
